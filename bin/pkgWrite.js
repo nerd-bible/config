96e326149cc8709c6c245cb2ccd1287bdc916046
thesmartwon <thesmartwon@pm.me>
2026-08-03 12:07:05 -0400

    publish add namespace

--- bin/pkgWrite.js
+++ bin/pkgWrite.js
@@ -4,6 +4,7 @@ import { execGit, tagCmd } from "./execGit.js";
 
 const server = process.env["SERVER_URL"] ?? process.env["GITHUB_SERVER_URL"];
 const repo = process.env["REPOSITORY"] ?? process.env["GITHUB_REPOSITORY"];
+const namespace = process.env["NAMESPACE"] ?? "@nerd-bible";
 
 if (!server || !repo) throw Error("Required SERVER_URL and REPOSITORY");
 
@@ -11,6 +12,7 @@ const version = execGit(`${tagCmd} --exact-match`, true);
 
 console.log("Writing package.json");
 const pkg = JSON.parse(readFileSync("package.json", "utf8"));
+if (pkg.name.startsWith(namespace)) pkg.name = namespace + "/" + pkg.name;
 pkg.version = version.substring(1);
 pkg.repository = { url: `${server}/${repo}` };
 writeFileSync("package.json", JSON.stringify(pkg, null, 2));
