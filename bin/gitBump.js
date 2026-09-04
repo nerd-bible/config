74e9022d2dae7b0a0fcf5120559e5feb1a157399
thesmartwon <thesmartwon@pm.me>
2026-09-04 13:27:55 -0400

    allow git bump to include suffix

--- bin/gitBump.js
+++ bin/gitBump.js
@@ -1,21 +1,33 @@
 #!/usr/bin/env node
 import { execGit, tagCmd } from "./execGit.js";
 
+const suffix = process.argv[2] ?? "";
+
 execGit("fetch --tags");
-if (execGit(`${tagCmd} --exact-match`, true))
+let version = execGit(`${tagCmd} --exact-match`, true);
+if (version)
 	throw Error(`Current commit already has tag ${version}`);
 
-const prevVersion = "v0.0.0-beta2";
-console.log("Bumping", prevVersion);
+version = execGit(tagCmd, true) ?? "v0.0.0";
+console.log("Bumping", version);
 
-const newVersion = prevVersion.replace(
+if (suffix && !version.includes(suffix)) {
+	version += `-${suffix}0`;
+	console.log("Added suffix", version);
+}
+
+let ranReplacer = false;
+version = version.replace(
 	/^(v.*)([0-9]+)$/,
-	(_, p1, p2) => p1 + (parseInt(p2) + 1)
+	(_, p1, p2) => {
+		ranReplacer = true;
+		return p1 + (parseInt(p2) + 1);
+	}
 );
-if (newVersion  == prevVersion)
-	throw Error(`invalid version number ${prevVersion}`);
+if (!ranReplacer)
+	throw Error(`couldn't find suffix digits to bump ${version}`);
 
-console.log("Tagging", newVersion);
-execGit(`tag ${newVersion}`);
+console.log("Tagging", version);
+execGit(`tag ${version}`);
 console.log("You probably want to push now");
 console.log("git push --tags origin master");
